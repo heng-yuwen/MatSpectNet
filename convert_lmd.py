17cf2c60e3613c71cd506dc96fd8fb18194a0a87
Yuwen Heng <y.heng@soton.ac.uk>
2023-07-27 15:08:50 +0800

    update convert_lmd.py

--- convert_lmd.py
+++ convert_lmd.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 import numpy as np
 from glob import glob
+import tqdm
 
 def load_and_save_mask_for_one_image(img_name, mask_paths, category2code):
     """load the original masks, and combine them into one for each image.
@@ -11,7 +12,7 @@ def load_and_save_mask_for_one_image(img_name, mask_paths, category2code):
     Args:
         img_name (str): the name of the RGB image.
     """
-    origin_img = Image.open(os.path.join("localmatdb/images", img_name)).convert('RGB')
+    origin_img = Image.open(os.path.join("data/localmatdb/images", img_name)).convert('RGB')
     segment_paths = [path for path in mask_paths if img_name in path]
 
     # get the category from path str like ./COCO_train2014_000000010073.jpg_food_mask.png
@@ -37,15 +38,16 @@ def load_and_save_mask_for_one_image(img_name, mask_paths, category2code):
 
     segment_img = Image.fromarray(segment_img)
     name = img_name.split("/")[-1][:-4]
-    segment_img.save("localmatdb/masks_png/{}.png".format(name))
+    segment_img.save("data/localmatdb/masks_png/{}.png".format(name))
 
 if __name__ == "__main__":
     """enumerate  the file names
     """
+    os.mkdir("data/localmatdb/masks_png")
     image_names = os.listdir("data/localmatdb/images")
     mask_paths = glob("data/localmatdb/masks/**/*.png", recursive=True)
     category2code = {"asphalt": 0, "ceramic": 1, "concrete": 2, "fabric": 3, "foliage": 4,
                               "food": 5, "glass": 6, "metal": 7, "paper": 8, "plaster": 9, "plastic": 10,
                               "rubber": 11, "soil": 12, "stone": 13, "water": 14, "wood": 15}
-    for img_name in image_names:
+    for img_name in tqdm.tqdm(image_names):
         load_and_save_mask_for_one_image(img_name, mask_paths, category2code)
