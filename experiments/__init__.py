abc35233d98d07df656da3cfa57373479c0e51f8
Yuwen Heng <hengyuwen@outlook.com>
2023-09-12 20:33:20 +0800

    upload spectral recovery code

---
+++ experiments/__init__.py
@@ -0,0 +1,10 @@
+from .spectral_recovery import SpectralRecovery
+from .matspectnet import MatSpectNet
+
+__all__ = {"SpectralRecovery": SpectralRecovery,
+           "MatSpectNet": MatSpectNet}
+
+
+def get_experiment(experiment_name):
+    assert experiment_name in __all__, f"The experiment {experiment_name} does not exist!"
+    return __all__[experiment_name]
